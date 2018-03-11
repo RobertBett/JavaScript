dd48e28e3e1de5f8367b2e9fb6c8913ddd961a41
Robert Bett <robertbett6@gmail.com>
2018-03-11 18:42:44 -0400

    custom directives added

--- directives-start/src/app/basic-highlight/basic-highlight.directive.ts
+++ directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,14 +1,21 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, HostListener, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[appBasicHighlight]'
 })
 export class BasicHighDirective implements OnInit{
-
-    constructor(private elementRef:ElementRef){
+    backgroundColor: string;
+    constructor(private elementRef:ElementRef, private renderer:Renderer2){
         elementRef.nativeElement
     }
     ngOnInit(){
-      this.elementRef.nativeElement.style.backgroundColor = 'green';
+    }
+
+    @HostListener('mouseenter') mouseover(eventData:Event){
+        this.renderer.setStyle(this.elementRef.nativeElement,'background-color', 'green')
+    }
+
+    @HostListener('mouseleave') mouseleave(eventData:Event){
+        this.renderer.setStyle(this.elementRef.nativeElement,'background-color', 'blue')
     }
 }
--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -4,31 +4,35 @@ import {
   Renderer2, 
   ElementRef, 
   HostListener, 
-  HostBinding} from '@angular/core';
+  HostBinding,
+  Input} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
+  @Input() defaultColor:string = 'transparent';
+  @Input() highlightColor:string = 'blue';
   //this can bind styles to elements and it ca bind to any property
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor;
   constructor(private elRef:ElementRef, private renderer:Renderer2) { }
 
   ngOnInit(){
     // this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue', false, false);
+    this.backgroundColor= this.defaultColor;
   }
 
   //can be triggered wheever some event occurs and the event 
   //is specified as an argument and string 
   @HostListener('mouseenter')mouseover(eventData:Event){
     // this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue', false, false);
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
 
   }
 
   @HostListener('mouseleave')mouseleave(eventData:Event){
     // this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent', false, false);
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
     }
 
 }
